43cd5573f321b28e7f1cd11a9752ba9a4ea817e1
Yury Kuznetsov <yurik@bluemarblegeo.com>
2025-01-21 09:00:37 -0500

    Update
    Changes to be committed:
    modified:   table_of_offsets.py
    modified:   tests/test_table_of_offsets.py
    modified:   tutorial/import_sample_data.ipynb

--- table_of_offsets.py
+++ table_of_offsets.py
@@ -1,13 +1,19 @@
 #
 #
 #
-__version__ = "0.5.0" 
+
+import ezdxf
+import numpy
+import pandas
+
+__version__ = "0.5.0"
+
 
 class TableOfOffsets:
     def __init__(self):
         pass
 
-    '''#      Sheer
+    """#      Sheer
 33       NaN
 32    1-10-2
 30     1-8-6
@@ -27,18 +33,19 @@ class TableOfOffsets:
 2     2-10-0
 0      3-0-7
 Name: 0, dtype: object
-'''
+"""
+
     @staticmethod
-    def verify_triplet(w : list):
+    def verify_triplet(w: list):
         return True
 
     @staticmethod
-    def offset2double(triplet : str):
+    def offset2double(triplet: str):
         delta = 0
-        if triplet[-1] == '-':
-            delta = -1.0 /16
+        if triplet[-1] == "-":
+            delta = -1.0 / 16
             triplet = triplet[:-1]
-        elif triplet[-1] == '+':
+        elif triplet[-1] == "+":
             delta = 1.0 / 16.0
             triplet = triplet[:-1]
 
@@ -54,3 +61,10 @@ Name: 0, dtype: object
         else:
             raise Exception("bad offset format")
         return d + delta
+
+
+class Model:
+    def __init__(self: object, _filename_csv: str):
+        self._filename_csv = _filename_csv
+        self._too = pandas.read_csv(self._filename_csv)
+        pass
--- tests/test_table_of_offsets.py
+++ tests/test_table_of_offsets.py
@@ -1,6 +1,11 @@
 from context import table_of_offsets
 
 import unittest
+import os
+import sys
+
+source_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
 
 class Test_TableOfOffsets(unittest.TestCase):
 
@@ -8,15 +13,26 @@ class Test_TableOfOffsets(unittest.TestCase):
         print(table_of_offsets.__version__)
         actual = table_of_offsets.TableOfOffsets()
         print(dir(actual))
-        self.assertEqual('foo'.upper(), 'FOO')
+        self.assertEqual("foo".upper(), "FOO")
 
     def test_offset2double(self):
         self.assertEqual(1.0, table_of_offsets.TableOfOffsets.offset2double("0-1-0"))
         self.assertEqual(1.0, table_of_offsets.TableOfOffsets.offset2double("0-1"))
         self.assertEqual(12.0, table_of_offsets.TableOfOffsets.offset2double("1-0"))
 
-        self.assertEqual(0.1875, table_of_offsets.TableOfOffsets.offset2double("0-0-1+"))
-        self.assertEqual(0.0625, table_of_offsets.TableOfOffsets.offset2double("0-0-1-"))
+        self.assertEqual(
+            0.1875, table_of_offsets.TableOfOffsets.offset2double("0-0-1+")
+        )
+        self.assertEqual(
+            0.0625, table_of_offsets.TableOfOffsets.offset2double("0-0-1-")
+        )
 
-if __name__ == '__main__':
+    def test_Model(self):
+        actual = table_of_offsets.Model(
+            os.path.join(source_directory, "sample_data/offset_table.csv")
+        )
+        print(type(actual._too))
+
+
+if __name__ == "__main__":
     unittest.main()
